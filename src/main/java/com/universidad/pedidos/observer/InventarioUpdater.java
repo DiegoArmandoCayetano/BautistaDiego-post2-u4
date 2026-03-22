2032d7d92901cade1e62ad045fce83d5e65d0d64
Tu Nombre <tuemailgmail.com>
2026-03-22 14:01:48 -0500

    geat: Agregar paquete observer con sus archivos en blanco, solo para establecer estructura

---
+++ src/main/java/com/universidad/pedidos/observer/InventarioUpdater.java
@@ -0,0 +1,5 @@
+package com.universidad.pedidos.observer;
+
+public class InventarioUpdater {
+    
+}
---
+++ src/main/java/com/universidad/pedidos/observer/PedidoConfirmadoEvent.java
@@ -0,0 +1,5 @@
+package com.universidad.pedidos.observer;
+
+public class PedidoConfirmadoEvent {
+    
+}
